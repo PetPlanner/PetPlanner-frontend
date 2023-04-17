109ab71a19247dfda1a328f5e0eb2adb08aa2032
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-04-17 23:01:28 +0200

    added auth context

--- app/src/layouts/header/index.tsx
+++ app/src/layouts/header/index.tsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import "./index.scss";
+import AuthContext from "../../store/login/AuthContext";
 const Header = () => {
+  const context = useContext(AuthContext);
   return (
     <div className={"header"}>
       <div className={"header-main"}>
         <div className={"header-main__logo"}>
           <div className={"header-main__logo-logo"}></div>
-          <div className={"header-main__logo-text"}>PetPlanner</div>
+          <div className={"header-main__logo-text"}>
+            PetPlanner {context.token}
+          </div>
         </div>
         <div className={"header-main__options"}>
           {/* {role === "REGISTERED" && getUserNavbar()}
