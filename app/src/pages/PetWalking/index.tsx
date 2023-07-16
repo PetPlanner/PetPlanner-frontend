6aa9619d7abd71bc764b519bdb2905ee1c5a7aac
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-07-16 14:33:00 +0200

    updated pet-walking feature

--- app/src/pages/PetWalking/index.tsx
+++ app/src/pages/PetWalking/index.tsx
@@ -1,37 +1,81 @@
-import { Circle, MapContainer, TileLayer, useMap } from "react-leaflet";
+import { Circle, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import Card from "../../components/Card";
 import "./index.scss";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import startIcon from "../../assets/images/owner.png";
+import walkerIcon from "../../assets/images/dog-walking (2).png";
 import "leaflet/dist/leaflet.css";
 import useRouteProtector from "../../utils/routeProtector/routeProtector";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../utils/store/AuthContext";
+import { findUserById, findWalkersByCity } from "../../services/userService";
+import { WarningMessage } from "../../utils/toastService/toastService";
+import NavButton from "../../components/Button";
 
-let StartIcon = L.icon({
-  iconUrl: startIcon,
+let WalkerIcon = L.icon({
+  iconUrl: walkerIcon,
   shadowUrl: iconShadow,
 });
+
 const WalkingPage = () => {
+  const [user, setUser] = useState();
+  const [walkers, setWalkers] = useState();
+  const contex = useContext(AuthContext);
   useRouteProtector({
     roles: ["CUSTOMER", "ADMIN", "DRIVER", "WALKER", "VET", "TRAINER"],
   });
-  const MapComponent = () => {
-    const map = useMap();
 
-    // Add routing control to the map when it is available
-    if (map && !map.hasLayer(routingControl as any)) {
-      routingControl.addTo(map);
+  const renderWalkers = () => {
+    let retVal = [];
+    for (let walker of walkers as any) {
+      retVal.push(
+        <Marker
+          icon={WalkerIcon}
+          position={[walker.address.lat, walker.address.lon]}
+        >
+          <Popup>
+            <div>
+              <h3 style={{ fontSize: "1.4rem" }}>Walker Details</h3>
+              <p style={{ fontSize: "1.2rem" }}>
+                Phone Number: {walker.phoneNumber}
+              </p>
+            </div>
+          </Popup>
+        </Marker>
+      );
     }
+    return retVal;
+  };
+  const fetchUser = async () => {
+    let res: any;
+    res = await findUserById(+contex.user.id);
+    if (!res || !res.data) {
+      WarningMessage("Something went wrong.");
+      return;
+    }
+    setUser(res.data);
+  };
 
-    return null;
+  useEffect(() => {
+    fetchUser();
+  }, [contex.user.id]);
+
+  const findWalkers = async () => {
+    let res: any;
+    res = await findWalkersByCity((user as any).address.city);
+    if (!res || !res.data) {
+      WarningMessage("Something went wrong");
+      return;
+    }
+    setWalkers(res.data);
+    console.log(res.data);
   };
 
   return (
     <div className="walking">
       <Card width="80vw" height="80vh" backgroundColor="rgb(240, 248, 255)">
         <div className="walking__container">
-          <div className="walking__container__content">bla</div>
           <div className="walking__container__map">
             <MapContainer
               center={[45.246219799792556, 19.851756995545784]}
@@ -39,50 +83,32 @@ const WalkingPage = () => {
               style={{ height: "100%", opacity: "1" }}
             >
               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              {/* <MapComponent /> */}
-              <Circle
-                color="red"
-                center={[45.246219799792556, 19.851756995545784]}
-                radius={500}
-              ></Circle>
+              {user && (
+                <Circle
+                  color="red"
+                  center={[
+                    (user as any).address.lat,
+                    (user as any).address.lon,
+                  ]}
+                  radius={500}
+                ></Circle>
+              )}
+              {walkers && renderWalkers()}
             </MapContainer>
+          </div>
+          <div className="walking__container__content">
+            <h1 id="pet-walking__title">Pet Walking Service</h1>
+            <div className="walking__container__content__btn">
+              <NavButton
+                text="Find A Dog Walker Near You!"
+                submitHandler={findWalkers}
+              ></NavButton>
+            </div>
           </div>
         </div>
       </Card>
     </div>
   );
 };
-
-const waypointIcon = L.icon({
-  iconUrl: "../../assets/images/owner.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
-
-const waypoints = [
-  {
-    latLng: L.latLng(45.246219799792556, 19.851756995545784),
-    icon: StartIcon,
-  },
-  {
-    latLng: L.latLng(45.246419799792556, 19.901758995545784),
-    icon: StartIcon,
-  },
-];
-
-const routingControl = L.Routing.control({
-  waypoints: waypoints,
-  routeWhileDragging: true,
-  lineOptions: {
-    styles: [
-      {
-        color: "blue",
-        opacity: 0.6,
-        weight: 4,
-      },
-    ],
-  } as any,
-});
 
 export default WalkingPage;
--- app/src/services/userService.tsx
+++ app/src/services/userService.tsx
@@ -3,3 +3,7 @@ import { HttpMethod, request } from "./HTTP";
 export async function findUserById(id: number) {
   return await request(`/user/${id}`);
 }
+
+export async function findWalkersByCity(city: string) {
+  return await request(`/user/city/${city}`);
+}
